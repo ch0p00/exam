04c20f100784b42e82b624e0f3a843238cb92abb
chop <o.panasiuk@internetdevels.com>
2023-07-05 17:56:44 +0300

    fixed bugs

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -704,7 +704,6 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * will allow the site to run off of all variants of example.com and
  * example.org, with all subdomains included.
  */
-$settings['trusted_host_patterns']=array('^exam\.docksal$');
 
 /**
  * The default list of directories that will be ignored by Drupal's file API.
